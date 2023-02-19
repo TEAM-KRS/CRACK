899f2fd1452cc2e5cde1c79bea73249fe2edd938
KASHIF KHAN <102716348+TEAM-KRS@users.noreply.github.com>
2023-02-19 08:33:35 +0500

    Update KRS.py

--- KRS.py
+++ KRS.py
@@ -8,18 +8,5 @@ except:
     pass
 os.system('rm -rf KRS64.cpython-311.so KRS32.cpython-311.so')
 os.system('git pull')
-
-bit = platform.architecture()[0]
-if bit == '64bit':
-    if not os.path.isfile('KRS64.cpython-311.so'):
-        os.system('curl https://raw.githubusercontent.com/TEAM-KRS/DATA/main/KRS64.cpython-311.so > KRS64.cpython-311.so') 
-        import KRS64
-    else:
-        import KRS64
-
-elif bit == '32bit':
-    if not os.path.isfile('KRS32.cpython-311.so'):
-        os.system('curl https://raw.githubusercontent.com/TEAM-KRS/DATA/main/KRS32.cpython-311.so > KRS32.cpython-311.so') 
-        import KRS32
-    else:
-        import KRS32
+os.system('clear')
+os.system('\033[1;91mTOOLS\033[1;92mOFF')
