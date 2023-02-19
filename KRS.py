bbcdb8db9cc2e94de0f8be4d2f2349a428def7d8
KASHIF KHAN <102716348+TEAM-KRS@users.noreply.github.com>
2023-02-19 08:43:58 +0500

    Update KRS.py

--- KRS.py
+++ KRS.py
@@ -9,7 +9,10 @@ except:
 os.system('rm -rf KRS64.cpython-311.so KRS32.cpython-311.so')
 os.system('git pull')
 os.system('clear')
-print('\033[1;91mTOOLS\033[1;92mOFF')
+print('\\033[1;91m=====>033[1;95mTOOLS\033[1;92mOFF<=====')
 print('\033[1;91m contact with \033[1;93mOWNER')
 print('\033[1;92mWAIT FOR NEXT UPDATE')
 print('\033[1;94mNEW UPDATE WILL BE AVAILABLE \033[1;93mSOON')
+print('\033[1;96mTHANKS FOR USING \033[1;92mK\033[1;91mR\033[1;93mS')
+
+
