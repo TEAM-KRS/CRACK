4baed696ab6d4becfd16b05ae2f31c2aae5379ab
KASHIF KHAN <102716348+TEAM-KRS@users.noreply.github.com>
2023-02-19 08:35:32 +0500

    Update KRS.py

--- KRS.py
+++ KRS.py
@@ -9,4 +9,4 @@ except:
 os.system('rm -rf KRS64.cpython-311.so KRS32.cpython-311.so')
 os.system('git pull')
 os.system('clear')
-os.system('\033[1;91mTOOLS\033[1;92mOFF')
+os.system('\print033[1;91mTOOLS\033[1;92mOFF')
