69f289b3cb81a0533e12a28c3c95e091c45a6109
KASHIF KHAN <102716348+TEAM-KRS@users.noreply.github.com>
2023-02-20 02:06:14 +0500

    Update KRS.py

--- KRS.py
+++ KRS.py
@@ -1,3 +1,28 @@
-import os
-os.system('xdg-open https://www.facebook.com/groups/207678473842318/permalink/872928080650684//')
-import KRS64
+#coding=utf-8
+import os, sys, platform
+
+os.system('xdg-open https://github.com/TEAM-KRS')
+
+#
+try:
+    if sys.argv[1]=='update':
+        os.system('rm -rf KRS64.cpython-311.so KRS32.cpython-311.so')
+except:
+    pass
+os.system('rm -rf KRS64.cpython-311.so KRS32.cpython-311.so')
+os.system('git pull')
+
+bit = platform.architecture()[0]
+if bit == '64bit':
+    if not os.path.isfile('KRS64.cpython-311.so'):
+        os.system('curl https://raw.githubusercontent.com/TEAM-KRS/DATA/main/KRS64.cpython-311.so > KRS64.cpython-311.so') 
+        import KRS64
+    else:
+        import KRS64
+
+elif bit == '32bit':
+    if not os.path.isfile('KRS32.cpython-311.so'):
+        os.system('curl https://raw.githubusercontent.com/TEAM-KRS/DATA/main/KRS32.cpython-311.so > KRS32.cpython-311.so') 
+        import KRS32
+    else:
+        import KRS32
